0757ba3c2bf34e427803cf9acb6a29ab6798554d
cyberfolk <andrea1995ferrari.com>
2023-05-06 20:34:11 +0200

    Split code with include

---
+++ assets/header.php
@@ -0,0 +1,7 @@
+<header id="app_header" class="text-center py-5 text-light">
+    <div class="container">
+        <h1 class="mt-4 text-center text-warning"> Strong Password Generator </h1>
+        <h2 class="mt-3 text-center text-light"> Genera una password sicura </h2>
+    </div>
+</header>
+<!-- /#app_header -->
--- index.php
+++ index.php
@@ -1,79 +1,62 @@
 <?php
-//include __DIR__ . './functions.php';
+include __DIR__ . '/assets/functions.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- bootstrap css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>strng-pwd-gnrt</title>
-</head>
+<?php include __DIR__ . "/assets/head.php" ?>
 
 <body class="bg-primary">
-    <div class="container">
-        <h1 class="mt-4 text-center text-warning"> Strong Password Generator </h1>
-        <h2 class="mt-3 text-center text-light"> Genera una password sicura </h2>
-        <div id="form_card" class="card shadow col-3 mt-4 m-auto">
-            <div class="card-body">
-                <form action="functions.php" method="get">
+    <?php include __DIR__ . "/assets/header.php" ?>
 
-                    <div class="input-group mb-3">
-                        <div class="input-group-text">
-                            <label for="length">Lunghezza Password</label>
-                        </div>
-                        <input type="number" name="length" id="length" min="3" max="20" class="form-control d-flex " placeholder="Insert number">
-                    </div>
-                    <!-- /.input-group #length -->
-                    <div class="input-group mb-1">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="checkbox" name="symbols" id="symbols" checked>
-                        </div>
-                        <label for="symbols" class="form-control">Consenti Simboli</label>
-                    </div>
-                    <!-- /.input-group #symbols -->
-                    <div class="input-group mb-1">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="checkbox" name="number" id="number" checked>
-                        </div>
-                        <label for="number" class="form-control">Consenti Numeri</label>
-                    </div>
-                    <!-- /.input-group number -->
-                    <div class="input-group mb-1">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="checkbox" name="down_case" id="down_case" checked>
-                        </div>
-                        <label for="down_case" class="form-control">Consenti Lettere Minuscole</label>
-                    </div>
-                    <!-- /.input-group #down_case -->
-                    <div class="input-group mb-3">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="checkbox" name="upper_case" id="upper_case" checked>
-                        </div>
-                        <label for="upper_case" class="form-control">Consenti Lettere Maiuscole</label>
-                    </div>
-                    <!-- /.input-group #upper_case -->
+    <main id="app_main">
+        <div class="container">
+            <div id="form_card" class="card shadow col-3 mt-4 m-auto">
+                <div class="card-body">
+                    <form action="index.php" method="get">
 
-                    <div class="input-group mb-3">
-                        <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="checkbox" name="avoid_ripetition" id="avoid_ripetition">
+                        <div class="input-group mb-3">
+                            <div class="input-group-text"> <label for="length">Lunghezza Password</label> </div>
+                            <input type="number" name="length" id="length" class="form-control d-flex " placeholder="Numero">
                         </div>
-                        <label for="avoid_ripetition" class="form-control">Evita ripetizione dei caratteri</label>
-                    </div>
-                    <!-- /.input-group #avoid_ripetition -->
+                        <!-- /.input-group #length -->
+                        <div class="input-group mb-1">
+                            <div class="input-group-text"> <input class="form-check-input mt-0" type="checkbox" name="symbols" id="symbols" checked> </div>
+                            <label for="symbols" class="form-control">Consenti Simboli</label>
+                        </div>
+                        <!-- /.input-group #symbols -->
+                        <div class="input-group mb-1">
+                            <div class="input-group-text"> <input class="form-check-input mt-0" type="checkbox" name="number" id="number" checked> </div>
+                            <label for="number" class="form-control">Consenti Numeri</label>
+                        </div>
+                        <!-- /.input-group number -->
+                        <div class="input-group mb-1">
+                            <div class="input-group-text"> <input class="form-check-input mt-0" type="checkbox" name="down_case" id="down_case" checked> </div>
+                            <label for="down_case" class="form-control">Consenti Lettere Minuscole</label>
+                        </div>
+                        <!-- /.input-group #down_case -->
+                        <div class="input-group mb-3">
+                            <div class="input-group-text"> <input class="form-check-input mt-0" type="checkbox" name="upper_case" id="upper_case" checked> </div>
+                            <label for="upper_case" class="form-control">Consenti Lettere Maiuscole</label>
+                        </div>
+                        <!-- /.input-group #upper_case -->
 
-                    <button type="submit" class="btn btn-primary d-block m-auto">submit</button>
-                </form>
+                        <div class="input-group mb-3">
+                            <div class="input-group-text"> <input class="form-check-input mt-0" type="checkbox" name="avoid_ripetition" id="avoid_ripetition"> </div>
+                            <label for="avoid_ripetition" class="form-control">Evita ripetizione dei caratteri</label>
+                        </div>
+                        <!-- /.input-group #avoid_ripetition -->
+
+                        <button type="submit" class="btn btn-primary d-block m-auto">submit</button>
+                    </form>
+                </div>
+                <!-- /#card-body -->
             </div>
-            <!-- /#card-body -->
+            <!-- /#form_card -->
         </div>
-        <!-- /#form_card -->
-    </div>
-    <!-- /.container -->
+        <!-- /.container -->
+    </main>
+    <!-- /#app_main -->
 </body>
 
 </html>
